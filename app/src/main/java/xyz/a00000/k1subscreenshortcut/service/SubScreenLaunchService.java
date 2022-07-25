e98669838a1db2fcbad23a9c309012ba21498928
A0000000000 <mengxin_zzl@outlook.com>
2022-07-26 00:32:36 +0800

    change display get way

--- app/src/main/java/xyz/a00000/k1subscreenshortcut/service/SubScreenLaunchService.java
+++ app/src/main/java/xyz/a00000/k1subscreenshortcut/service/SubScreenLaunchService.java
@@ -5,7 +5,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.util.Log;
 import android.view.Display;
 
 import androidx.annotation.Nullable;
